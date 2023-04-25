7f74e40f70f97f1ed39489749026c97a7def3da7
Daniel Mattox <dmattox10@gmail.com>
2023-04-25 15:35:24 -0400

    Update index.js
    Security!

--- bazinga-games/index.js
+++ bazinga-games/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const cors = require('cors');
-const stripe = require('stripe')('sk_test_51M6yNAChZbepu0tegirpoYXAh8qTu81xCEJQJuKVPkuHeSAVBH0NWxx0juJZNuFNdAtNYTl8QRTgyThIh1ABqwUt00IWs0OqDY')
+const stripe = require('stripe')('sk_test_51M6yNAChZbepu0tegirpoYXAh8qTu81xCEJQJuKVPkuHeSAVBH0NWxx0juJZNuFNdAtNYTl8QRTgyThIh1ABqwUt00IWs0OqDY') // Even if it's a test key, delete this key from your account and switch to using ENV file!
 const uuid = require('uuid')
 const PORT = 5000;
 const express = require('express');
