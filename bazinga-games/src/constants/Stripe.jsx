feaa3be73b473de4ea477bafa38600fe11ff6af2
Daniel Mattox <dmattox10@gmail.com>
2023-04-25 16:15:55 -0400

    Update Stripe.jsx
    Security!

--- bazinga-games/src/constants/Stripe.jsx
+++ bazinga-games/src/constants/Stripe.jsx
@@ -3,3 +3,5 @@ const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
   : 'pk_test_51M6yNAChZbepu0teU0TYJC8vE6MTAyXu7VAaCMYndlFPi6Fv1sYhkG8UnHNoIOKQadMZTy9B7qTM6GO3e21dEhPs00mwOEgotQ';
 
 export default STRIPE_PUBLISHABLE;
+
+// Even test keys need to use ENV file.
